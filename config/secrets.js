1420c4ef5092d1a29f0e8a0f7e5080e06d48b64c
Luis Hernandez <luishrd@gmail.com>
2019-03-07 07:42:55 -0700

    update secrets

--- config/secrets.js
+++ config/secrets.js
@@ -1,4 +1,6 @@
+// add a .env file with the JWT_SECRET you'd like to use
+
 module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'add a third table for many to many',
-  twilioApiKey: process.env.TWILIO_API_KEY || 'key here',
+  jwtSecret:
+    process.env.JWT_SECRET || "learn to code, pay $0 until you're hired",
 };
